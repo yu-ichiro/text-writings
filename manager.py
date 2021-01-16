c6b5493d1242d9cfe82d264c48328ddf8dd7eb78
Yuichiro Smith <contact@yu-smith.com>
2021-01-16 20:21:09 +0900

    add manager.py

---
+++ manager.py
@@ -0,0 +1,23 @@
+import json
+import os
+import shutil
+
+
+if __name__ == "__main__":
+    with open("_index.json") as index:
+        settings = json.loads(index.read())
+    if os.path.exists("tags"):
+        shutil.rmtree("tags")
+    os.mkdir("tags")
+    for tag, items in settings["tags"].items():
+        if not os.path.exists(f"tags/{tag}"):
+            os.mkdir(f"tags/{tag}")
+        for item in items:
+            if not os.path.exists(f"tags/{tag}/{item}"):
+                os.mkdir(f"tags/{tag}/{item}")
+
+    for data_id, info in settings["data"].items():
+        for tag, items in info["tags"].items():
+            for item in items:
+                src = os.path.abspath(info["path"])
+                os.symlink(src, f"tags/{tag}/{item}/{data_id}.md")
---
+++ tags/platform/zenn/20200116-tmux-ansi-color.md
@@ -0,0 +1,1 @@
+/Users/yuichiro/ghq/github.com/yu-ichiro/text-writings/master_data/20210116-tmux-ansi-color.md
